4e11d7aee8bdf34a86b90c83f40adefbf95dab8b
Anandhu Lal k <92375127+anandhulk@users.noreply.github.com>
2022-11-22 21:28:56 +0530

    Update SharedPreferencesMainActivity.java

--- SharedPreferencesMainActivity.java
+++ SharedPreferencesMainActivity.java
@@ -1,43 +1,59 @@
-package com.example.sharedpreferencesactivity;
+package com.example.sharedpreference;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText name,age,phoneno;
+    EditText first;
+    EditText last,num;
+    public static final String SHARED="sharedprefs";
+    public static final String NAME="nm";
+    public static final String LAST="ls";
+    public static final String DIG="digit";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        name=(EditText)findViewById(R.id.ee1);
-        age=(EditText) findViewById(R.id.ee2);
-        phoneno=(EditText)findViewById(R.id.ee3);
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        SharedPreferences sh = getSharedPreferences("sp",MODE_PRIVATE);
-        String n=sh.getString("name","");
-        int a=sh.getInt("age",0);
-        int phone=sh.getInt("phoneno",0);
-
+        first=findViewById(R.id.first);
+        last=findViewById(R.id.last);
+        num=findViewById(R.id.number);
 
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        SharedPreferences sharedPreferences = getSharedPreferences("sp",MODE_PRIVATE);
-        SharedPreferences.Editor sp = sharedPreferences.edit();
-        sp.putString("name",name.getText().toString());
-        sp.putInt("age",Integer.parseInt(age.getText().toString()));
-        sp.putInt("phoneno",Integer.parseInt(phoneno.getText().toString()));
-        sp.commit();
-        sp.apply();
+        Toast.makeText(this, "ON...pause..", Toast.LENGTH_SHORT).show();
+        SharedPreferences sh = getSharedPreferences(SHARED,MODE_PRIVATE);
+        SharedPreferences.Editor edit=sh.edit();
+        edit.putString(NAME,first.getText().toString());
+        edit.putString(LAST,last.getText().toString());
+        edit.putInt(DIG,Integer.parseInt(num.getText().toString()));
+        //sp.commit();
+        edit.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "ON...resume", Toast.LENGTH_SHORT).show();
+        SharedPreferences sh=getSharedPreferences(SHARED,MODE_PRIVATE);
+        String str=sh.getString(NAME,"");
+        String str1=sh.getString(LAST,"");
+        int a=sh.getInt(DIG,0);
+
+        first.setText(str);
+        last.setText(str1);
+        num.setText(String.valueOf(a));
     }
 }
